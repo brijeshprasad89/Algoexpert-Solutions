2398c04b16723c9f6cd9fecd557c1a15f5687a97
Brijesh <brijeshprasad1989@gmail.com>
2020-12-01 19:23:18 +0100

    add CloseseValueBST algo

--- src/com/challenges/algoexpert/easy/CloseseValueBST.java
+++ src/com/challenges/algoexpert/easy/CloseseValueBST.java
@@ -31,7 +31,33 @@ package com.challenges.algoexpert.easy;
 public class CloseseValueBST {
 
     public static void main(String[] args) {
+        BST node1 = new BST(1);
+        BST node2 = new BST(2);
+        BST node3 = new BST(5);
+        BST node4 = new BST(5);
+        BST tree = new BST(10);
+        BST node6 = new BST(14);
+        BST node7 = new BST(13);
+        BST node8 = new BST(22);
+        BST node9 = new BST(15);
 
+        node1.left = null;
+        node1.right = null;
+        node2.left = node1;
+        node2.right = null;
+        node3.left = node2;
+        node3.right = node4;
+        tree.left = node3;
+        node6.left = null;
+        node6.right = null;
+        node7.left = null;
+        node7.right = node6;
+        node8.left = null;
+        node8.right = null;
+        node9.right = node8;
+        node9.left = node7;
+        tree.right = node9;
+        System.out.print(findClosestValueInBst(tree, 12));
     }
 
     static class BST {
@@ -45,19 +71,26 @@ public class CloseseValueBST {
     }
 
     public static int findClosestValueInBst(BST tree, int target) {
-        // Write your code here.
-        int min = -1;
-        BST left = tree.left;
-        BST right = tree.right;
-        while (null != left && null != right) {
-
-        }
-
-        return -1;
+        return findClosestValueInBst(tree, target, tree.value);
     }
 
-    private void traverseTree(BST node) {
+    private static int findClosestValueInBst(BST tree, int target, int closest) {
 
+        BST currentNode = tree;
+        while (null != currentNode) {
+            if (Math.abs(target - closest) > Math.abs(target - currentNode.value)) {
+                closest = currentNode.value;
+            }
+            if (target < currentNode.value) {
+                currentNode = currentNode.left;
+            } else if (target > currentNode.value) {
+                currentNode = currentNode.right;
+            } else {
+                break;
+            }
+        }
+
+        return closest;
     }
 
 }
